2385cf1085881b6a7981cd82a27b2f5b0b2749ea
zayetsA <artem.zaiets@onix-systems.com>
2023-07-31 17:48:13 +0300

    test game web view event

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -349,6 +349,9 @@ class GameScene extends Phaser.Scene {
         this.gameOverText.setText('Game Over\nScore: ' + this.score);
         this.gameOverText.setVisible(true)
         this.scoreText.setVisible(false)
+        if (typeof AndroidInterface !== "undefined") {
+            AndroidInterface.onGameLoose(this.score);
+        }
     }
 
     destroyCoin(coin, platform) {
